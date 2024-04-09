058b52f902e9a09c4d571e6530beaa66905e171f
Ilia Akatov <ilia-akatoff@ya.ru>
2024-04-09 21:34:57 +0300

    Hotfix: -fix result_final -fix readme.md (description)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Working_With_Branches
+# Two funcs for convert and merge branches!!! 
--- str_func.py
+++ str_func.py
@@ -2,13 +2,13 @@ def word_upper(word='Word'):
     """
     Converts to words with upper letters.
     """
-    result = word.upper()
-    return result
+    result_final = word.upper()
+    return result_final
 
 
 def word_title(word='Word'):
     """
     Converts to words with title letter.
     """
-    result = word.title()
-    return result
+    result_final = word.title()
+    return result_final
